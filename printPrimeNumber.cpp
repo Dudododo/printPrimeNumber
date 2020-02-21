0212622f7228b9f20a6e6baa9332cc233ecbf831
Dudododo <a752601260@gmail.com>
2020-02-21 15:30:56 +0800

    无

--- printPrimeNumber.cpp
+++ printPrimeNumber.cpp
@@ -1,20 +1,41 @@
-﻿// printPrimeNumber.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <stdlib.h>
 
-#include <iostream>
+using namespace std;
+
+
+//0：不是素数
+//1：是素数
+int checkPrimeNumber(int num)
+{
+	if (num == 0 || num == 1)
+		return 0;
+	else
+	{
+		for (int i = 2; i <= sqrt(num); i++)
+		{
+			if (num % i == 0)
+				return 0;
+		}
+	}
+	return 1;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int totalnum = 100;
+
+	for (int i = 0; i < totalnum; i+=2)
+	{
+		int res = checkPrimeNumber(i);
+		if (res == 1)
+		{
+			cout << "the primenumber is:" << i << endl;
+		}
+		if (i == 2) //从2之后只检验奇数
+			i = 1;
+	}
+	
+	system("pause");
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
